badf687280ae00bcb2e9dc51ccdf70b5e75d7049
Jesus Abellan <jesusabellan97@gmail.com>
2023-11-08 13:39:47 +0100

    basic detail pet view added

--- adoptame/pet/models.py
+++ adoptame/pet/models.py
@@ -10,6 +10,10 @@ class Specie(models.Model):
     def __str__(self):
         return self.name
 
+class Race(models.Model):
+    name = models.CharField("Raza", max_length=50)
+    specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
+
 class Pet(models.Model):
     name = models.CharField('Name of the pet',  max_length=50)
     age = models.IntegerField('Age in years', default=0)
@@ -18,4 +22,5 @@ class Pet(models.Model):
     create_at = models.DateField(auto_now_add=False)
     created_by = models.ForeignKey(User, verbose_name="Owner", related_name='pets', on_delete=models.CASCADE)
     specie = models.ForeignKey(Specie, related_name='pets', verbose_name="Pet's specie", on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, related_name='pets', verbose_name="Pet's race", null=True, on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='pet_images', blank=True, null=True)
--- adoptame/pet/views.py
+++ adoptame/pet/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Pet
 
-# Create your views here.
+def detail(request, pk):
+    pet = get_object_or_404(Pet, pk=pk)
+    context = {'pet': pet}
+
+    return render(request, 'pet/detail.html', context)
