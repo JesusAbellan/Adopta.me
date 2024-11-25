bbf28edb50ecfa0c74bbeb70491d473b0196aab0
Jesus Abellan <jesusabellan97@gmail.com>
2024-11-25 19:04:11 +0100

    fixed login and registered forms

--- adoptame/core/forms.py
+++ adoptame/core/forms.py
@@ -6,37 +6,40 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+    FORMCLASS ='col-12 rounded-3'
 
     username = forms.CharField(widget=forms.TextInput(attrs={
                                 'placeholder': 'Tu nombre de usuario',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
     email = forms.CharField(widget=forms.EmailInput(attrs={
                                 'placeholder': 'Tu dirección de correo',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
                                 'placeholder': 'Tu contraseña',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
                                 'placeholder': 'Repite tu contraseña',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
 
 
 class LoginForm(AuthenticationForm):
+    FORMCLASS ='col-12 rounded-3'
+
     username = forms.CharField(widget=forms.TextInput(attrs={
                                 'placeholder': 'Tu nombre de usuario',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
                                 'placeholder': 'Tu contraseña',
-                                'class': 'col-12 rounded-3 border-0',
+                                'class': FORMCLASS,
                                 'style': 'height:3em'
                                 }))
