cfc1d380dbdf21c00aa7c45695c1880725886a56
Jesus Abellan <jesusabellan97@gmail.com>
2024-11-28 11:53:14 +0100

    change db to postgresql

--- adoptame/adoptame/settings.py
+++ adoptame/adoptame/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'adoptame.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adoptame',
+        'USER': 'postgres',
+        'PASSWORD': 'Hermenegildo97'
     }
 }
 
